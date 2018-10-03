67f7ac66479dd9185febb34fdf6d46c22b43fd06
sejanH <sejan840@gmail.com>
2018-10-04 02:13:28 +0600

    03-10-2018

---
+++ main/renderer.js
@@ -0,0 +1,6 @@
+'use strict'
+window.$ = window.jQuery = require('jquery')
+window.popper = require('popper.js')
+window.Bootstrap = require('bootstrap')
+    // require('../node_modules/bootstrap/dist/js/bootstrap.min.js')
+require('simple-scrollbar')
